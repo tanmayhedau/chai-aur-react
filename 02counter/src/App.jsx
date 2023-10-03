568a2a01e4fc90f24f89b7fc03bb4a166be75b4e
tanmay <tanmay@flokq.com>
2023-10-03 22:42:00 +0530

    counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  function handleClickForIncrement() {
+    setValue((v) => v + 1);
+  }
+
+  function handleClickForDecrement() {
+    setValue((v) => v - 1);
+  }
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value: {value}</h2>
+      <button onClick={handleClickForIncrement}>Add value</button>
+      <br></br>
+      <br />
+      <button onClick={handleClickForDecrement}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
