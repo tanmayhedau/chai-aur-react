479498c6bd15a7eeae86a4e60079e060ccd18b38
tanmay <tanmay@flokq.com>
2023-11-11 23:22:22 +0530

    new commit

---
+++ 16reduxToolKitToDo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn Redux Toolkit with TODO</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
